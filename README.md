9ed25659b2cf1623ac5a4d9ca62c0949aece126b
antonovmike <antonovma@outlook.com>
2023-08-20 17:51:59 +0400

    stop if the address is invalid

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# http_healthchecker
+# http_healthcheck_python
+This app sends an HTTP GET request at specified intervals
+
+The check is performed in a cycle with a specified interval.
+
+There are three possible test results:
+1. `OK(200)`, in case the request returned HTTP status code `200`.
+2. `ERR({HTTP_CODE})`, in case the request returned an HTTP status code other than `200`.
+3. `URL parsing error`, in case the second argument is not a valid HTTP URL. Then the application ends.
+ 
+The utility takes two arguments:
+1. Integer value of the interval in seconds.
+2. HTTP URL to be checked.
 
 Install and activate Python virtual environment:
 ```bash
@@ -21,4 +33,8 @@ python main.py 2 http://httpstat.us/500
 ```
 ```bash
 python main.py 3 not_an_address
+```
+
+```bash
+deactivate
 ```
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import argparse
 import time
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
+from urllib.parse import urlparse
 
 
 class HealthChecker:
     @staticmethod
-    def check_url(interval, url):
+    def check_url(url):
+        parsed_url = urlparse(url)
+        if not all([parsed_url.scheme, parsed_url.netloc]):
+            raise ValueError("URL parsing error")
+
         try:
-            with urlopen(url, timeout=interval) as connection:
+            with urlopen(url, timeout=3) as connection:
                 code = connection.getcode()
                 return f"Checking '{url}'. Result: OK({code})"
         except HTTPError as e:
@@ -19,6 +24,7 @@ class HealthChecker:
             return f"Checking '{url}'. Result: ERR({str(e)})"
 
 
+
 def main():
     parser = argparse.ArgumentParser(description='HTTP Health Checker')
     parser.add_argument('interval', type=int, help='Interval between checks in seconds')
@@ -26,11 +32,16 @@ def main():
     args = parser.parse_args()
 
     checker = HealthChecker()
-    while True:
-        result = checker.check_url(args.interval, args.url)
-        print(result)
-        time.sleep(args.interval)
 
+    while True:
+        try:
+            result = checker.check_url(args.url)
+            print(result)
+            time.sleep(args.interval)
+        except ValueError as e:
+            print(str(e))
+            break
 
 if __name__ == '__main__':
     main()
+
